ee6bfe068ce941470cb81f6b672c1ed6ac1ab7c5
Zi-Max <katakuricharlottesama12@gmail.com>
2023-12-13 08:15:46 +0200

    Update parse_content
    - fixed some bugs in the prase heading&paragraph
    -- if the user added a heading that above h5 the article will broke
    -- if the user add a newline between paragraph the parser will combine
    --- them into 1 paragraph element instead of 2 elements
    - clean the parse_content file split the code into function and remove
    -- unnecessary code

--- site_packge/static/js/parse_content.js
+++ site_packge/static/js/parse_content.js
@@ -1,24 +1,32 @@
 let content_container = document.getElementById("article"),
   article_elements = [],
-  element,
-  paragrah = document.createElement("p"),
-  unlist = document.createElement("ul");
+  url = "/api/v1.0/article/",
+  id_split = window.location.href.split("/"),
+  article_id = id_split[id_split.length - 1];
 
-function parse_list(element) {
-  let text = element.slice(1, element.length),
-    listElement;
+function parse_list(elements, current_index) {
+  let unlist = document.createElement("ul"),
+    i = 0;
 
-  listElement = document.createElement("li");
-  listElement.textContent = text;
+  for (; i + current_index < elements.length; i++) {
+    let element = elements[i + current_index].replaceAll("\r", ""),
+      element_text = element.slice(1, element.length);
+    if (element[0] != "-") break;
 
-  return listElement;
+    listElement = document.createElement("li");
+    listElement.textContent = element_text;
+
+    unlist.appendChild(listElement);
+  }
+
+  return [unlist, i - 1];
 }
 
 function parse_heading(element) {
-  let heading = element.slice(0, 6);
+  let heading = element.slice(0, 7);
 
   for (let i = 0; i < heading.length; i++) {
-    if (heading[i] != "#") {
+    if (heading[i] != "#" || i == 6) {
       let head_element = document.createElement(`h${i}`);
       head_element.textContent = element.slice(i, element.length);
 
@@ -37,79 +45,59 @@ function parse_note(element, ele_class) {
   return noteElement;
 }
 
-function parseContent() {
-  let url = "/api/v1.0/article/",
-    article_id;
+function parse_paragraph(elements, current_index) {
+  let paragraph = document.createElement("p"),
+    i = 0;
 
-  id_split = window.location.href.split("/");
-  article_id = id_split[id_split.length - 1];
+  for (; i + current_index < elements.length; i++) {
+    let element = elements[i + current_index].replaceAll("\r", "");
 
-  fetch(`${url}${article_id}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.text();
-    })
-    .then((data) => {
-      let article_split = data.split("\n");
-      for (let i = 0; i < article_split.length; i++) {
-        if (article_split[i] == "\r") continue;
+    if (["#", "-", "!", "@", "("].includes(element[0]) || !element[0]) break;
 
-        element = article_split[i].replaceAll("\r", "");
+    let brElement = document.createElement("br"),
+      textNode = document.createTextNode(element);
 
-        if (element[0] == "#") {
-          if (paragrah.children.length != 0) {
-            article_elements.push(paragrah.cloneNode(true));
-            paragrah.textContent = "";
-          } else if (unlist.children.length != 0) {
-            article_elements.push(unlist.cloneNode(true));
-            unlist.textContent = "";
-          }
-          article_elements.push(parse_heading(element));
-        } else if (element[0] == "-") {
-          if (paragrah.children.length != 0) {
-            article_elements.push(paragrah.cloneNode(true));
-            paragrah.textContent = "";
-          }
-          unlist.appendChild(parse_list(element));
-        } else if (element[0] == "@" || element[0] == "!") {
-          if (paragrah.children.length != 0) {
-            article_elements.push(paragrah.cloneNode(true));
-            paragrah.textContent = "";
-          } else if (unlist.children.length != 0) {
-            article_elements.push(unlist.cloneNode(true));
-            unlist.textContent = "";
-          }
+    paragraph.appendChild(textNode);
+    paragraph.appendChild(brElement);
+  }
 
-          let ele_class = element[0] == "@" ? "info" : "danger";
-
-          article_elements.push(parse_note(element, ele_class));
-        } else {
-          if (unlist.children.length != 0) {
-            article_elements.push(unlist);
-            unlist.textContent = "";
-          }
-          paragrah.append(element);
-          paragrah.appendChild(document.createElement("br"));
-        }
-      }
-
-      if (paragrah.children.length != 0) {
-        article_elements.push(paragrah.cloneNode(true));
-        paragrah.textContent = "";
-      } else if (unlist.children.length != 0) {
-        article_elements.push(unlist.cloneNode(true));
-        unlist.textContent = "";
-      }
-
-      for (let i = 0; i < article_elements.length; i++) {
-        content_container.appendChild(article_elements[i]);
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  return [paragraph, i - 1];
 }
 
-parseContent();
+fetch(`${url}${article_id}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.text();
+  })
+  .then((data) => {
+    let article_split = data.split("\n");
+
+    for (let i = 0; i < article_split.length; i++) {
+      if (article_split[i] == "\r") continue;
+      let element = article_split[i].replaceAll("\r", "");
+
+      if (element[0] == "#") article_elements.push(parse_heading(element));
+      else if (element[0] == "@")
+        article_elements.push(parse_note(element, "info"));
+      else if (element[0] == "!")
+        article_elements.push(parse_note(element, "danger"));
+      else if (element[0] == "-") {
+        let parsed_list = parse_list(article_split, i);
+        article_elements.push(parsed_list[0]);
+        i += parsed_list[1];
+      } else {
+        let parsed_paragraph = parse_paragraph(article_split, i);
+        article_elements.push(parsed_paragraph[0]);
+        i += parsed_paragraph[1];
+      }
+    }
+
+    for (let i = 0; i < article_elements.length; i++) {
+      content_container.appendChild(article_elements[i]);
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
